72ff743c9a35c5e4693812b14f0e0c50e5278af8
Naveed-Iqbal-Devops-Engineer <naveediqbalit@gmail.com>
2025-12-25 14:22:57 +0500

    Update deployApp.groovy

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -5,7 +5,4 @@ def call(String composeFile, String branch) {
         docker-compose -f ${composeFile} --env-file .env up -d --remove-orphans
     """
 }
-7. cleanupImages.groovy:
-def call() {
-    sh 'docker rmi ${BACKEND_TAG_DH} ${FRONTEND_TAG_DH} || true'
-}
+
