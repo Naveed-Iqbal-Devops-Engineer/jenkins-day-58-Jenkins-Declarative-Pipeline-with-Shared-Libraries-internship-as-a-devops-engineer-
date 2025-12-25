985178dd8b89b39b191b433215ab1f9db9aff08c
Naveed-Iqbal-Devops-Engineer <naveediqbalit@gmail.com>
2025-12-25 14:20:45 +0500

    Update pushImages.groovy

--- vars/pushImages.groovy
+++ vars/pushImages.groovy
@@ -2,9 +2,3 @@ def call() {
     sh 'docker push ${BACKEND_TAG_DH}'
     sh 'docker push ${FRONTEND_TAG_DH}'
 }
-5. prepareEnvFile.groovy:
-def call() {
-    writeFile file: '.env', text: """BACKEND_IMAGE=${BACKEND_TAG_DH}
-FRONTEND_IMAGE=${FRONTEND_TAG_DH}
-"""
-}
